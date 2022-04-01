97afa0c28e83c8d7fff3e76a9cb4392e9c0c059e
Sameer Nazeer <sameernln@gmail.com>
2022-04-01 17:14:18 +0530

    Adding Steps

--- Feature/OperateRover.feature
+++ Feature/OperateRover.feature
@@ -1,9 +1,13 @@
 Feature: Operate the rover
+
 Scenario Outline: Move the rover to the target location
-Given Rover is at <x>, <y>
-And the rover is facing <direction>
-When rover is given <commands>
-Then rover should be at <newX> <newY>
-And rover should be facing <newDirection>
-Examples:
-|x|y|direction|commands|newX|newY|newDirection|
+	Given Rover is at <x>, <y>
+	And the rover is facing <direction>
+	When rover is given <commands>
+	Then rover should be at <newX> <newY>
+	And rover should be facing <newDirection>
+
+	Examples:
+		| x | y | direction | commands   | newX | newY | newDirection |
+		| 1 | 2 | N         | LMLMLMLMM  | 1    | 3    | N            |
+		| 3 | 3 | E         | MMRMMRMRRM | 5    | 1    | E            |
---
+++ Steps/MoveRoverSteps.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace MarsRovers.Steps
+{
+    class MoveRoverSteps
+    {
+        private readonly ScenarioContext _scenarioContext;
+
+        public MoveRoverSteps(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+        [Given(@"Rover is at (.*), (.*)")]
+        public void GivenRoverIsAt(int p0, int p1)
+        {
+            _scenarioContext.Pending();
+        }
+
+        [Given(@"rover is pointing towards E")]
+        public void GivenRoverIsPointingTowardsE()
+        {
+            _scenarioContext.Pending();
+        }
+
+        [When(@"rover moves forward")]
+        public void WhenRoverMovesForward()
+        {
+            _scenarioContext.Pending();
+        }
+
+        [Then(@"rover should be at (.*)")]
+        public void ThenRoverShouldBeAt(string p0)
+        {
+            _scenarioContext.Pending();
+        }
+
+        [Then(@"rover should be facing E")]
+        public void ThenRoverShouldBeFacingE()
+        {
+            _scenarioContext.Pending();
+        }
+    }
+}
